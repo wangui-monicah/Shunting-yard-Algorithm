045fe23d4752f83956eccdb43377fcfdcacabce6
wangui-monicah <humphreygicheru@Humphreys-MacBook-Pro.local>
2022-02-07 17:10:03 -0500

    data structures and algos

--- Projects/ShuntingYardAlgo/src/MyLinkedList/SimpleLinkedList.java
+++
@@ -1,9 +0,0 @@
-package MyLinkedList;
-public interface SimpleLinkedList<AnyType> {
-	public void insert(AnyType x);
-	public void delete(AnyType x);
-	public boolean contains(AnyType x);
-	public AnyType lookup(AnyType x);
-	public boolean isEmpty();
-	public void printList();
-}
--- Projects/ShuntingYardAlgo/src/MyQueue/Queue.java
+++
@@ -1,7 +0,0 @@
-package MyQueue;
-public interface Queue<AnyType> {
-	public boolean isEmpty();
-	public void enqueue(AnyType x);
-	public AnyType dequeue();
-	public AnyType peek();
-}
